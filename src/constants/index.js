222b0d457ca2ac12fe4dccf70ee01e0ab05e5ee4
Arka-saha10 <sahaarka10@gmail.com>
2024-11-23 12:49:06 +0530

    Personal Projects Update

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,9 +3,9 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 2 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 2 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
@@ -41,32 +41,32 @@ Platform for data storage.`,
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "BlackBox Chatting Website",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "This product brings privacy to a different level while having a private chat with an individual or a team. It is simply a website where one can chat freely which gets clears as soon as the user Logs out of their system. It leverages Node.js and React.js library to achieve a serverless website where one can join a room using unique room code. Cookies are being accessed to store the messages temporarily which takes out the question of Server out of the equation.",
+    technologies: ["HTML", "CSS", "React.js", "Node.js", "Web-Socket"],
   },
   {
-    title: "Task Management App",
+    title: "Blogging Website with Google OAuth 2.0",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "Developed a full-fledged Blogging Website from scratch using MERN Stack and Hosted the same using Heroku. Technologies used were React, React Router, Node.js, Material UI, Google OAuth 2.0, Google Cloud Console, MongoBD Atlas, Github Actions. The also had high responsiveness and was mobile, desktop and tablet responsive. Implemented features like social logins, authentication, authorization, session management, state management etc.",
+    technologies: ["MongoDB", "Express", "Node.js", "React.js","Google OAuth"],
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["HTML", "Tailwind-CSS", "React.js", "Bootstrap"],
   },
   {
-    title: "Blogging Platform",
+    title: "dotOne | HighRadius Winter Intern Project",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "Developed a full-fledged MVC model and AI Integrated Website using Springboot and React to handle huge amount of Transaction data for the fintech company from scratch. Developed under strict supervision of HighRadius Technologies, so it was developed with Corporate Coding Standards and with proper Authentication and Authorization.",
+    technologies: ["React.js", "JDBC", "Python", "Flask", "mySQL"],
   },
 ];
 
